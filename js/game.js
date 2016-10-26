1e4eb304b69091fda0421303049e27961e0cdeff
Jessica Dembe <jmdembe@gmail.com>
2016-10-26 14:13:45 -0400

    Removed extra lines of code from game file and added jshint file

--- js/game.js
+++ js/game.js
@@ -6,10 +6,16 @@
 
     document.querySelector('button').addEventListener('click', function buttonClicked() {
 
+        var diceTotal = (dieNumOne+dieNumTwo);
+        var diceRolls = 0;
+        var dieNumOne = document.querySelector('.dice aside:first-child').innerText;  // 3
+        var dieNumTwo = document.querySelector('.dice aside:last-child').innerText;
+        var gameStartTime = document.querySelector('.game-start').innerText
+
         var dice = {
             start: new Date(),
-            // rounds: [
-            //   {currentRound: new Date(),
+            rounds: [{
+                currentRound: new Date(),
             //   rounds: 0;
             // }],
             roll: function roll() {
@@ -17,16 +23,12 @@
                 num = Math.ceil(Math.random() * 6);
                 return num;
             }
-
         }
         // var x = 13;
         //              |--------------------------------x------------------------|
-        var dieNumOne = document.querySelector('.dice aside:first-child').innerText;  // 3
-        // document.querySelector('.dice aside:first-child').innerText = 42
 
-        var dieNumTwo = document.querySelector('.dice aside:last-child').innerText;
 
-        document.querySelector('.game-start').innerText='Game Started '+ dice.start;
+        gameStartTime='Game Started '+ dice.start;
         console.log(dice.start);
 
         document.querySelector('.dice aside:first-child').innerText = dice.roll();  // 5 3 1 6
@@ -35,10 +37,8 @@
         dieNumOne=parseInt(document.querySelector('.dice aside:first-child').innerText);
         dieNumTwo=parseInt(document.querySelector('.dice aside:last-child').innerText);
 
+        diceTotal = (dieNumOne+dieNumTwo)
 
-        var diceTotal = (dieNumOne+dieNumTwo);
-
-        var diceRolls = 0;
 
         if (diceTotal === 7 || diceTotal === 11) {
             diceRolls++;
